7486c88b09598f48ff5962ef30b644bd2da0c483
lulu-moon <lulumoon2014@gmail.com>
2023-08-18 12:40:57 +0100

    work started on appsync program

--- appsync.py
+++ appsync.py
@@ -1,4 +1,17 @@
 import tkinter as tk
 
-root= tk.TK()
+root= tk.Tk()
 root.title('AppSync')
+root.geometry('800x600')
+
+
+def bank():
+    print('Bank')
+
+
+mainTitle = tk.Label(root, text="App Sync")
+zbankButton = tk.Button(root,text="ZBANK LINK ", height=60,width=120)
+
+mainTitle.grid(row=0,column=0,rowspan=3)
+
+root.mainloop()
